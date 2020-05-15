8387dea17b7d4f5cffb25394d8373de2b30b9f14
Daianes26 <daianeasilva26@gmail.com>
2020-05-15 14:40:51 -0400

    Working with the repository dictionary

--- python_repos.py
+++ python_repos.py
@@ -7,6 +7,22 @@ print('Status code:', r.status_code)
 
 #Store api response in a variable
 response_dict = r.json()
+print('Total repositories: ', response_dict['total_count'])
 
-#Process results
-print(response_dict.keys())
+#Explore information about the respositories
+repo_dicts = response_dict['items']
+print('Repositories returned: ', len(repo_dicts))
+
+#Examine the first repository
+repo_dict = repo_dicts[0]
+# print('\nKeys: ', len(repo_dict))
+# for key in sorted(repo_dict.keys()):
+#     print(key)
+print('\nSelected information about first repository: ')
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
