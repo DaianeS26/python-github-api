4c28af07d6dab696f880d1994a56bf80bb7b45ee
Daianes26 <daianeasilva26@gmail.com>
2020-05-14 23:57:27 -0400

    First call

---
+++ python_repos.py
@@ -0,0 +1,12 @@
+import requests
+
+#Make API call and store the response
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('Status code:', r.status_code)
+
+#Store api response in a variable
+response_dict = r.json()
+
+#Process results
+print(response_dict.keys())
